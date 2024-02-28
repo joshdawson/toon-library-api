5eec4513a9889f6a678772ec2730dacb6057e3da
Josh Dawson <joshtdawson11@gmail.com>
2024-02-28 11:30:09 +0000

    Add data ingestor

---
+++ apps/toon-library-data-ingestor/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app/app.module';
+import { AppService } from './app/app.service';
+
+async function bootstrap() {
+  const app = await NestFactory.createApplicationContext(AppModule);
+  const appService = app.get(AppService);
+
+  console.log(appService.getData());
+
+}
+
+bootstrap();
