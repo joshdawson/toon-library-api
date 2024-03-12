5f398f54b65d9f00fbf25d50126d40bdd5a036ec
Josh Dawson <joshtdawson11@gmail.com>
2024-03-12 11:20:06 +0000

    Add API security PRO-10 (#4)
    * feat: add security headers via helmet
    
    * feat: add GraphQL query depth limit
    
    * feat: add rate limiting

--- apps/toon-library-api/src/app/app.module.ts
+++ apps/toon-library-api/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ThrottlerModule } from '@nestjs/throttler';
+import depthLimit from 'graphql-depth-limit';
 import * as path from 'path';
 
 import { APIModule } from '../api/api.module';
@@ -11,7 +13,12 @@ import { APIModule } from '../api/api.module';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
+      validationRules: [depthLimit(10)],
     }),
+    ThrottlerModule.forRoot([{
+      ttl: 60,
+      limit: 60,
+    }]),
   ],
   controllers: [],
   providers: [],
