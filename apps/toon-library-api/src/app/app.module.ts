4a599323f6d429b8e3c7b36af7817292d77cf96b
Josh Dawson <joshtdawson11@gmail.com>
2024-03-12 11:13:54 +0000

    feat: add rate limiting

--- apps/toon-library-api/src/app/app.module.ts
+++ apps/toon-library-api/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ThrottlerModule } from '@nestjs/throttler';
 import depthLimit from 'graphql-depth-limit';
 import * as path from 'path';
 
@@ -14,6 +15,10 @@ import { APIModule } from '../api/api.module';
       autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
       validationRules: [depthLimit(10)],
     }),
+    ThrottlerModule.forRoot([{
+      ttl: 60,
+      limit: 60,
+    }]),
   ],
   controllers: [],
   providers: [],
